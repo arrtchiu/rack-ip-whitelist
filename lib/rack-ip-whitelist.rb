19d6b39494114f3d6ed018d0555bb283abf6c997
arrtchiu <arrtchiu@gmail.com>
2014-03-10 19:51:09 +0800

    use x-forwarded-for

--- lib/rack-ip-whitelist.rb
+++ lib/rack-ip-whitelist.rb
@@ -11,13 +11,21 @@ module Rack
       if white_listed?(env)
         @app.call(env)
       else
-        [ 200, {"Content-Type" => "text/html"}, "<p>You are not authorized to view this site</p>" ]
+        [ 401, {"Content-Type" => "text/plain"}, ["Unauthorized."] ]
       end
     end
     
     def white_listed?(env)
-      Rails.logger.info "WHITELIST: ENV: #{env["REMOTE_ADDR"].inspect}"
-      Rails.env.production? ? @ip_addresses.include?(env["REMOTE_ADDR"]) : true
+      Rails.logger.info "WHITELIST: ENV: #{remote_address(env)}"
+      Rails.env.production? ? @ip_addresses.include?(remote_address(env)) : true
+    end
+    
+    def remote_address(env)
+      if env["HTTP_X_FORWARDED_FOR"].blank?
+        env["REMOTE_ADDR"]
+      else
+        env["HTTP_X_FORWARDED_FOR"]
+      end
     end
   end
 end
